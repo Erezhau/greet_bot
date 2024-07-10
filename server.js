0a636238f354b6b5026bc0a98b1935cc2b5121e2
Joshua <parabellum1789@gmail.com>
2024-07-10 15:43:11 +0600

    Add server.js package-lock.json

---
+++ server.js
@@ -0,0 +1,7 @@
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf('7383860888:AAFHTfouibAuGcl0BGYnkQ56Q9w8aQrMWrM')
+bot.start((ctx) => ctx.reply('Welcome to AritmetiClash! Start the Mini App by pressing the blue "Play now" utton below. This is an early version, but you can either play the test mode, or register to save your score. If you run into any problems, please contact @chhat_promotion_bot. Have fun!'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
