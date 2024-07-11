afc9719b676a5384ccb948dc882417cd28c1d584
Joshua <parabellum1789@gmail.com>
2024-07-11 14:40:08 +0600

    Add server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf("7383860888:AAFHTfouibAuGcl0BGYnkQ56Q9w8aQrMWrM")
 bot.start((ctx) => ctx.reply('Welcome to AritmetiClash! Start the Mini App by pressing the blue "Play now" bnutton below. This is an early version, but you can either play the test mode, or register to save your score. If you run into any problems, please contact @chaat_promotion_bot. Have fun!'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+
 
 const PORT = process.env.PORT || 3000;
 bot.launch({
